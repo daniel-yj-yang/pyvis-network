7fa89b752c24139b8c2fa2b640dd2b425f29ee9b
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-12-30 13:41:03 -0600

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -7,4 +7,6 @@
 from pyvis_network import network, dataset
 
 nodes_df, edges_df = dataset().load_as_df('Example1')
-network().add_df(nodes_df=nodes_df, edges_df=edges_df).write_html('Example1.html')
+graph1 = network().add_df(nodes_df=nodes_df, edges_df=edges_df)
+graph2 = graph1.clone_graph()
+graph2.write_html('Example1.html')
