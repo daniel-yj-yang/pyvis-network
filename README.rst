203439dfee4b833cb8472e43b40881ab4e2b27cd
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-12-30 13:39:53 -0600

    Update

--- README.rst
+++ README.rst
@@ -38,7 +38,9 @@ Sample Usage
 
 >>> from pyvis_network import network, dataset
 >>> nodes_df, edges_df = dataset().load_as_df("Example1")
->>> network(title="Example1").add_df(nodes_df=nodes_df,edges_df=edges_df).show("example1.html")
+>>> graph1 = network(title="Example1").add_df(nodes_df=nodes_df,edges_df=edges_df)
+>>> graph2 = graph1.clone_graph()
+>>> graph2.show("example1.html")
 
 >>> from pyvis_network import network, dataset
 >>> nodes_df, edges_df = dataset().load_as_df("Machine_Learning")
--- pyvis_network/network/_network.py
+++ pyvis_network/network/_network.py
@@ -5,10 +5,30 @@
 # License: BSD-3-Clause
 
 
-from typing import Union, List
+from typing import Union
 from pathlib import Path
 import pandas as pd
 import webbrowser
+
+
+class Edge(dict):
+  def __init__(self, source_node: 'Node' = None, to_node: 'Node' = None, weight: float = 1.0, *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self['source_node'] = source_node
+    self['to_node'] = to_node
+    self['weight'] = weight
+
+
+class Node(dict):
+  def __init__(self, label: str = '', shape: str = 'circle', *args, **kwargs):
+    super().__init__(*args, **kwargs)
+    self['label'] = label
+    self['shape'] = shape
+    self['edges'] = dict() # key: to_Node['label'], value: Edge
+  
+  def add_edge(self, to_node: 'Node' = None):
+    if to_node['label'] not in self['edges']:
+      self['edges'][to_node['label']] = Edge(source_node=self, to_node=to_node)
 
 
 class network(object):
@@ -17,22 +37,44 @@ class network(object):
       self.title = title
       self.width = width
       self.height = height
-      self.nodes_hashmap = dict()
+      self.nodes = dict() # key: label, value = Node
 
-    def add_node(self, node_id: Union[float, int, str] = None, shape: str = 'circle'):
-      if node_id not in self.nodes_hashmap:
-        self.nodes_hashmap[node_id] = {'shape': shape, 'to': dict()}
+    def add_node(self, label: Union[float, int, str] = None):
+      if label not in self.nodes:
+        self.nodes[label] = Node(label = label)
 
-    def add_edge(self, from_node_id: Union[float, int, str] = None, to_node_id: Union[float, int, str] = None, weight: float = 1.0):
-      if to_node_id not in self.nodes_hashmap[from_node_id]['to']:
-        self.nodes_hashmap[from_node_id]['to'][to_node_id] = {'weight': weight,}
+    def add_edge(self, source_label: Union[float, int, str] = None, to_label: Union[float, int, str] = None):
+      if source_label in self.nodes and to_label in self.nodes:
+        self.nodes[source_label].add_edge(to_node = self.nodes[to_label])
+      else:
+        print('Error: the nodes are not created yet')
 
     def add_df(self, nodes_df: pd.DataFrame, edges_df: pd.DataFrame) -> None:
       for idx, row in nodes_df.iterrows():
-        self.add_node(node_id = row['label'])
+        self.add_node(label = row['label'])
       for idx, row in edges_df.iterrows():
-        self.add_edge(from_node_id=row['from'], to_node_id=row['to'])
+        self.add_edge(source_label=row['source'], to_label=row['to'])
       return self
+
+    def clone_graph(self):
+      new_graph = network()
+      original_to_clone_mapping = {} # key: id() of original node, value: cloned node
+      def clone_node(node: 'Node' = None) -> 'Node':
+        nonlocal original_to_clone_mapping, new_graph
+        if not node:
+          return None
+        if id(node) in original_to_clone_mapping:
+          return original_to_clone_mapping[id(node)]
+        cloned_node = Node(label=node['label'], shape=node['shape'])
+        new_graph.nodes[cloned_node['label']] = cloned_node
+        original_to_clone_mapping[id(node)] = cloned_node
+        if node['edges']:
+          for this_to_node_label in node['edges']:
+            cloned_node.add_edge(to_node=clone_node(self.nodes[this_to_node_label]))
+        return cloned_node
+      if self.nodes:
+        clone_node(list(self.nodes.values())[0])
+      return new_graph
 
     def _generate_html(self) -> None:
       """
@@ -61,20 +103,20 @@ class network(object):
   // create an array with nodes
   var nodes = new vis.DataSet(["""
       label_to_id_hashmap = {}
-      for idx, node_id in enumerate(self.nodes_hashmap):
-        label_to_id_hashmap[node_id] = idx+1
+      for idx, node_label in enumerate(self.nodes):
+        label_to_id_hashmap[node_label] = idx+1
         id_part = f"id: {idx+1}, "
-        label_part = f"label: \"{node_id}\""
+        label_part = f"label: \"{node_label}\""
         self.html += f"""
     {{ {id_part}{label_part} }},"""
       self.html += f"""
   ]);
   // create an array with edges
   var edges = new vis.DataSet(["""
-      for from_node_id in self.nodes_hashmap:
-        from_part = f"from: {label_to_id_hashmap[from_node_id]}, "
-        for to_node_id in self.nodes_hashmap[from_node_id]['to']:
-          to_part = f"to: {label_to_id_hashmap[to_node_id]}"
+      for source_label in self.nodes:
+        from_part = f"from: {label_to_id_hashmap[source_label]}, "
+        for to_node_label in self.nodes[source_label]['edges']:
+          to_part = f"to: {label_to_id_hashmap[to_node_label]}"
           self.html += f"""
     {{ {from_part}{to_part} }},"""
       self.html += f"""
