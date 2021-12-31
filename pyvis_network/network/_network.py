028fe83c1f063c3b3348f97eac9f6b34108ca8a8
Daniel Yang <3014022+daniel-yj-yang@users.noreply.github.com>
2021-12-30 18:21:39 -0600

    Update

--- pyvis_network/network/_network.py
+++ pyvis_network/network/_network.py
@@ -20,15 +20,16 @@ class Edge(dict):
 
 
 class Node(dict):
-  def __init__(self, label: str = '', shape: str = 'circle', *args, **kwargs):
+  def __init__(self, node_id: Union[float, int, str] = None, label: str = '', shape: str = 'circle', *args, **kwargs):
     super().__init__(*args, **kwargs)
+    self['node_id'] = node_id
     self['label'] = label
     self['shape'] = shape
-    self['edges'] = dict() # key: label, value: Edge
+    self['edges'] = dict() # key: to_node_id, value: Edge
   
   def add_edge(self, to_node: 'Node' = None):
-    if to_node['label'] not in self['edges']:
-      self['edges'][to_node['label']] = Edge(source_node=self, to_node=to_node)
+    if to_node['node_id'] not in self['edges']:
+      self['edges'][to_node['node_id']] = Edge(source_node=self, to_node=to_node)
 
 
 class network(object):
@@ -37,23 +38,23 @@ class network(object):
       self.title = title
       self.width = width
       self.height = height
-      self.nodes = dict() # key: label, value = Node
+      self.nodes = dict() # key: node_id, value = Node object
 
-    def add_node(self, label: Union[float, int, str] = None):
-      if label not in self.nodes:
-        self.nodes[label] = Node(label = label)
+    def add_node(self, node_id: Union[float, int, str] = None, label: str = ''):
+      if node_id not in self.nodes:
+        self.nodes[node_id] = Node(node_id = node_id, label = label)
 
-    def add_edge(self, source_label: Union[float, int, str] = None, to_label: Union[float, int, str] = None):
-      if source_label in self.nodes and to_label in self.nodes:
-        self.nodes[source_label].add_edge(to_node = self.nodes[to_label])
+    def add_edge(self, source_node_id: Union[float, int, str] = None, to_node_id: Union[float, int, str] = None):
+      if source_node_id in self.nodes and to_node_id in self.nodes:
+        self.nodes[source_node_id].add_edge(to_node = self.nodes[to_node_id])
       else:
         print('Error: the nodes are not created yet')
 
     def add_df(self, nodes_df: pd.DataFrame, edges_df: pd.DataFrame) -> None:
       for idx, row in nodes_df.iterrows():
-        self.add_node(label = row['label'])
+        self.add_node(node_id = row['node_id'], label = row['node_id'])
       for idx, row in edges_df.iterrows():
-        self.add_edge(source_label=row['source'], to_label=row['to'])
+        self.add_edge(source_node_id=row['source_node_id'], to_node_id=row['to_node_id'])
       return self
 
     def clone_graph(self):
@@ -65,12 +66,12 @@ class network(object):
           return None
         if id(node) in original_to_clone_mapping:
           return original_to_clone_mapping[id(node)]
-        cloned_node = Node(label=node['label'], shape=node['shape'])
-        new_graph.nodes[cloned_node['label']] = cloned_node
+        cloned_node = Node(node_id=node['node_id'], label=node['label'], shape=node['shape'])
+        new_graph.nodes[cloned_node['node_id']] = cloned_node
         original_to_clone_mapping[id(node)] = cloned_node
         if node['edges']:
-          for this_to_node_label in node['edges']:
-            cloned_node.add_edge(to_node=clone_node(self.nodes[this_to_node_label]))
+          for to_node_id in node['edges']:
+            cloned_node.add_edge(to_node=clone_node(self.nodes[to_node_id]))
         return cloned_node
       if self.nodes:
         clone_node(list(self.nodes.values())[0])
@@ -102,21 +103,23 @@ class network(object):
 <script type="text/javascript">
   // create an array with nodes
   var nodes = new vis.DataSet(["""
-      label_to_id_hashmap = {}
-      for idx, node_label in enumerate(self.nodes):
-        label_to_id_hashmap[node_label] = idx+1
-        id_part = f"id: {idx+1}, "
-        label_part = f"label: \"{node_label}\""
+      node_id_to_id_mapping = {}
+      idx = 1
+      for node_id in self.nodes:
+        node_id_to_id_mapping[node_id] = idx
+        node_id_part = f"id: {idx}, "
+        idx += 1
+        label_part = f"label: \"{self.nodes[node_id]['label']}\""
         self.html += f"""
-    {{ {id_part}{label_part} }},"""
+    {{ {node_id_part}{label_part} }},"""
       self.html += f"""
   ]);
   // create an array with edges
   var edges = new vis.DataSet(["""
-      for source_label in self.nodes:
-        from_part = f"from: {label_to_id_hashmap[source_label]}, "
-        for to_node_label in self.nodes[source_label]['edges']:
-          to_part = f"to: {label_to_id_hashmap[to_node_label]}"
+      for node_id in self.nodes:
+        from_part = f"from: {node_id_to_id_mapping[node_id]}, "
+        for to_node_id in self.nodes[node_id]['edges']:
+          to_part = f"to: {node_id_to_id_mapping[to_node_id]}"
           self.html += f"""
     {{ {from_part}{to_part} }},"""
       self.html += f"""
